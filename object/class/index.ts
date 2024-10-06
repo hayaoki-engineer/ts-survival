41c361309ade0c11e5e70dacb8b14cf24ef4087e
Taisei <taisei.960420@gmail.com>
2024-10-07 07:52:07 +0900

    フィールドの初期化

--- object/class/index.ts
+++ object/class/index.ts
@@ -1,40 +1,73 @@
-// クラス
-class Person { }
+/*
+ * クラス
+ */
+class Person {}
 const person = new Person();
-console.log(person) // Person {}
+console.log(person); // Person {}
 
-// コンストラクタ => クラスを new したときに実行される関数
+/*
+ * コンストラクタ
+ */
 class Person2 {
   constructor(name: string) {
-    console.log(`${name}です`)
+    console.log(`${name}です`);
   }
 }
-new Person2("Alice")
+new Person2("Alice");
 
-// フィールド
+/*
+ * フィールド
+ */
 class Person3 {
   name: string | undefined;
 }
 const alice1 = new Person3();
 alice1.name = "Alice";
-console.log(alice1) // Person3 { name: 'Alice' }
+console.log(alice1); // Person3 { name: 'Alice' }
 
-/// コンストラクタを用いたフィールドの初期化
+// コンストラクタを用いたフィールドの初期化
 class Person4 {
   name: string;
 
   constructor() {
-    this.name = "Alice"
+    this.name = "Alice";
   }
 }
 
-/// コンストラクタに引数を持たせてフィールドを動的に
+// コンストラクタに引数を持たせてフィールドを動的に
 class Person5 {
   name: string;
 
   constructor(personName: string) {
-    this.name = personName
+    this.name = personName;
   }
 }
 const alice2 = new Person5("三苫");
-console.log(alice2) // Person5 { name: '三苫' }
+console.log(alice2); // Person5 { name: '三苫' }
+
+/*
+ * メソット
+ */
+class Greeter {
+  greet(name: string): string {
+    return `Hello, ${name}!`;
+  }
+}
+const greeter = new Greeter();
+const greeting = greeter.greet("John");
+console.log(greeting);
+
+/*
+ * フィールドの初期化
+ */
+class Point {
+  // 初期化されていない状態
+  x: number;
+  y: number;
+
+  constructor() {
+    // x, yを初期化
+    this.x = 0;
+    this.y = 0;
+  }
+}
